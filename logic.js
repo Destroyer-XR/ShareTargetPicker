ddf34cefa81a4df028118a0cab587ed8f94eb56d
Destroyer-XR <overkillrobot@gmail.com>
2025-02-15 21:20:20 +0700

    Update logic.js

--- logic.js
+++ logic.js
@@ -267,23 +267,7 @@ function shareMessage() {
             {
                 "type": "flex",
                 "altText": "this is a flex message",
-                "contents": {
-                    "type": "bubble",
-                    "body": {
-                        "type": "box",
-                        "layout": "vertical",
-                        "contents": [
-                            {
-                                "type": "text",
-                                "text": "hello"
-                            },
-                            {
-                                "type": "text",
-                                "text": "world"
-                            }
-                        ]
-                    }
-                }
+                "contents": data
             }
         ],
         {
